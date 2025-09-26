629445a34d388e07c32242909ab8fbb5c285ae58
Joan Rodrigues <joanlima2005@gmail.com>
2025-09-26 15:50:16 +0200

    Project

---
+++ src/test/java/com/example/nurses/NursesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nurses;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NursesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

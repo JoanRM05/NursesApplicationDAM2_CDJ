a8e995090f5e2bd934761e832dfddf0fee5952e3
Joan Rodrigues <joanlima2005@gmail.com>
2025-10-23 16:23:42 +0200

    Added Pom dependecies, modified class Nurses and repository created #11

---
+++ src/main/java/com/example/nurses/Repository/NurseRepository.java
@@ -0,0 +1,10 @@
+package com.example.nurses.Repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.example.nurses.Entity.Nurse;
+
+@Repository
+public interface NurseRepository extends JpaRepository<Nurse, Long> {
+
+}

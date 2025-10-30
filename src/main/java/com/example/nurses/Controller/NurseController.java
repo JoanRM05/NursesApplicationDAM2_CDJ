908f4f288af92291a7a3348777310e46b52c9c32
cassiuste <cassiustedesco1@gmail.com>
2025-10-30 16:09:05 +0100

    feat: Added update endpoint in NurseController #24

--- src/main/java/com/example/nurses/Controller/NurseController.java
+++ src/main/java/com/example/nurses/Controller/NurseController.java
@@ -14,7 +14,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -64,6 +66,39 @@ public class NurseController {
 		  return ResponseEntity.ok(true);
 		} else {
 		  return ResponseEntity.ok(false);
+		}
+	}
+	
+	@PutMapping("/{id}")
+	public ResponseEntity<Void> updateNurse(@PathVariable long id, @RequestBody Nurse updatedNurse){
+		try {
+			Optional<Nurse> originalNurse = nurseRepository.findById(id);
+			if(originalNurse.isPresent()) {
+				Nurse nurse = originalNurse.get();
+	            if (updatedNurse.getName() != null) {
+	                nurse.setName(updatedNurse.getName());
+	            }
+	            if (updatedNurse.getUser() != null) {
+	                nurse.setUser(updatedNurse.getUser());
+	            }
+	            if (updatedNurse.getEmail() != null) {
+	                nurse.setEmail(updatedNurse.getEmail());
+	            }
+	            if (updatedNurse.getSurname() != null) {
+	                nurse.setSurname(updatedNurse.getSurname());
+	            }
+	            if (updatedNurse.getPass() != null) {
+	                nurse.setPass(updatedNurse.getPass());
+	            }				
+				nurseRepository.save(nurse);	
+				return ResponseEntity.ok().build();
+			}
+			else {
+				return ResponseEntity.notFound().build();
+			}
+		}
+		catch (Exception e) {
+			return ResponseEntity.badRequest().build();
 		}
 	}
 }
